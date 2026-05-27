b05ed0e9cfb590bd8d3c409daeb663ed5c9ce80f
MrPowerGamerBR <git@mrpowergamerbr.com>
2026-05-27 19:03:38 -0300

    Change Theme colors

--- app/src/main/kotlin/net/perfectdreams/butterscotch/android/components/ButterscotchTopBar.kt
+++ app/src/main/kotlin/net/perfectdreams/butterscotch/android/components/ButterscotchTopBar.kt
@@ -5,12 +5,12 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,8 +24,10 @@ fun ButterscotchTopBar(
     TopAppBar(
         title = { Text(title) },
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color(0xFFE8A552),
-            titleContentColor = Color.Black
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
         ),
         navigationIcon = navigationIcon,
         actions = actions,
--- app/src/main/kotlin/net/perfectdreams/butterscotch/android/theme/Color.kt
+++ app/src/main/kotlin/net/perfectdreams/butterscotch/android/theme/Color.kt
@@ -2,10 +2,16 @@ package net.perfectdreams.butterscotch.android.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val ButterscotchPrimary = Color(0xFFE8A552)
+val ButterscotchOnPrimary = Color(0xFF3A2A10)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ButterscotchSecondary = Color(0xFFD9C2A0)
+val ButterscotchOnSecondary = Color(0xFF3A2A10)
+
+val ButterscotchTertiary = Color(0xFF3A8C8C)
+val ButterscotchOnTertiary = Color(0xFFE8F4F4)
+
+val ButterscotchBackground = Color(0xFF1F1A14)
+val ButterscotchSurface = Color(0xFF2A231A)
+val ButterscotchOnBackground = Color(0xFFF1E6D4)
+val ButterscotchOnSurface = Color(0xFFF1E6D4)
