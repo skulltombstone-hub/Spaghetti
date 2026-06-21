ec012b6788c18e2a76b7977449f2ff0d383aca82
MrPowerGamerBR <git@mrpowergamerbr.com>
2026-06-21 17:29:38 -0300

    Version 2026-06-21.1 (Butterscotch-only changes)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "net.perfectdreams.butterscotch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 31
-        versionName = "2026.06.18-1"
+        versionCode = 32
+        versionName = "2026.06.21-1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
