b7a53b80f7741f5259c0b4cbfa856978a88c3496
Miguel C. Dias <skulltombstone@gmail.com>
2026-07-06 22:55:08 -0300

    Refactor GameRuntime interface to remove unnecessary indentation

--- app/src/main/kotlin/net/perfectdreams/butterscotch/android/runtime/GameRuntime.kt
+++ app/src/main/kotlin/net/perfectdreams/butterscotch/android/runtime/GameRuntime.kt
@@ -1,8 +1,6 @@
 package net.perfectdreams.butterscotch.android.runtime
 
 interface GameRuntime {
-
-        val kind: RuntimeKind
-
-            val capabilities: RuntimeCapabilities
+    val kind: RuntimeKind
+    val capabilities: RuntimeCapabilities
 }
