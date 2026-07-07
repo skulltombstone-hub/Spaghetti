b0f4ece0f1c7a6a155feb61f4771376dee07b583
Miguel C. Dias <skulltombstone@gmail.com>
2026-07-07 01:37:19 +0000

    Add runtime architecture base

---
+++ app/src/main/kotlin/net/perfectdreams/butterscotch/android/runtime/GameRuntime.kt
@@ -0,0 +1,9 @@
+package net.perfectdreams.butterscotch.android.runtime
+
+interface GameRuntime {
+
+        val kind: RuntimeKind
+
+            val capabilities: RuntimeCapabilities
+}
+}
---
+++ app/src/main/kotlin/net/perfectdreams/butterscotch/android/runtime/RuntimeCapabilities.kt
@@ -0,0 +1,17 @@
+package net.perfectdreams.butterscotch.android.runtime
+
+data class RuntimeCapabilities(
+
+        val supportsTouch: Boolean = true,
+
+            val supportsKeyboard: Boolean = true,
+
+                val supportsMouse: Boolean = false,
+
+                    val supportsGamepad: Boolean = true,
+
+                        val supportsVirtualControls: Boolean = true,
+
+                            val supportsSaveSlots: Boolean = true
+)
+)
