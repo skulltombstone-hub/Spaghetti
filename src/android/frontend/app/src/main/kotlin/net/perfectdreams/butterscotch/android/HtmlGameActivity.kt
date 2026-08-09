821fb4af55417722b146b44c961859f4fa716d19
Miguel C. Dias <skulltombstone@gmail.com>
2026-08-08 22:23:02 -0300

    Add BoxWithConstraintsScope import to HtmlGameActivity

--- src/android/frontend/app/src/main/kotlin/net/perfectdreams/butterscotch/android/HtmlGameActivity.kt
+++ src/android/frontend/app/src/main/kotlin/net/perfectdreams/butterscotch/android/HtmlGameActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
